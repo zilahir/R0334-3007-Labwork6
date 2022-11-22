18066073abffd2f2aa4b36d6b05e86c4b08cb574
Richard Zilahi <richard.zilahi@varjo.com>
2022-11-22 19:45:43 +0200

    splash screen init

---
+++ src/pages/Splash/index.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+const SplashScreen = (): ReactElement => (
+    <div>
+        <p>
+            splash screen
+        </p>
+    </div>
+)
+
+export default SplashScreen;

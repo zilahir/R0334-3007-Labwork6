22f139ed94cbc44538513ff4412724e31b2e98ae
Richard Zilahi <richard.zilahi@varjo.com>
2022-11-22 20:59:48 +0200

    implementing header component

--- src/components/Layout/components/Header/index.tsx
+++ src/components/Layout/components/Header/index.tsx
@@ -1,16 +1,34 @@
-import { IonHeader } from "@ionic/react";
+import { IonButton, IonHeader, IonIcon } from "@ionic/react";
 import { ReactElement } from "react";
+import { returnUpBackOutline } from "ionicons/icons"
+import styled from "styled-components"
 
 import styles from "./Header.module.scss";
 
-const Header = (): ReactElement => (
+interface IHeader {
+    rightIcon?: string
+    rightIconOnclick?: () => void
+}
+
+const StyledButton = styled.button`
+    font-size: 30px;
+    background: transparent;
+`
+
+const Header = ({ rightIcon, rightIconOnclick }: IHeader): ReactElement => (
     <IonHeader className={styles.headerRootContainer}>
-        <p>
-            hello fom header
-        </p>
-        <p>
-            hello fom header
-        </p>
+        <div>
+            <StyledButton onClick={(): void => alert("hello")} >
+                <IonIcon icon={returnUpBackOutline} />
+            </StyledButton>
+        </div>
+        {
+            (rightIcon && rightIconOnclick) && (
+                <StyledButton onClick={(): void => alert("hello")} >
+                    <IonIcon icon={rightIcon} />
+                </StyledButton>
+            )
+        }
     </IonHeader>
 )
 

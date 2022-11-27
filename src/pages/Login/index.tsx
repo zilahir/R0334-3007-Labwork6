44365ae930375e64beef388db2df54e2b305e543
Richard Zilahi <richard.zilahi@varjo.com>
2022-11-27 13:56:13 +0200

    adjusting layout component

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,10 +1,13 @@
 import { ReactElement } from "react"
+
 import Button from "../../components/Button"
 import Layout from "../../components/Layout"
 
+import styles from "./Login.module.scss";
+
 const Login = (): ReactElement => (
     <Layout>
-        <div>
+        <div className={styles.loginRootContainer}>
             <Button
                 label="Login"
                 onClick={(): void => alert("login")}

837ca2c29a5e6173bb6e1f34918f65660e3fbedf
Richard Zilahi <richard.zilahi@varjo.com>
2022-12-06 00:52:28 +0200

    placing order

--- src/pages/ProductPage/index.tsx
+++ src/pages/ProductPage/index.tsx
@@ -1,11 +1,30 @@
+import { IonCard, IonCardHeader } from "@ionic/react";
 import { ReactElement } from "react";
+
 import Layout from "../../components/Layout";
 import ProductSelector from "../../components/ProductSelector";
+import { useAppSelector } from "../../store/hooks";
+import { CartState } from "../../store/reducers/cart";
+import styles from "./ProductPage.module.scss";
+import { countTotalSum } from "../../store/reducers/cart/selectors"
 
-const ProductPage = (): ReactElement => (
-    <Layout>
-        <ProductSelector />
-    </Layout>
-)
+const ProductPage = (): ReactElement => {
+    const { products } = useAppSelector((store): CartState => store.cart)
+    return (
+        <Layout>
+            <IonCard className={styles.totalContainer}>
+                <IonCardHeader className={styles.header}>
+                    <h4>
+                        Total items: {countTotalSum(products).numOfProducts}
+                    </h4>
+                    <h3>
+                        Total sum {countTotalSum(products).totalSum} €
+                    </h3>
+                </IonCardHeader>
+            </IonCard>
+            <ProductSelector />
+        </Layout>
+    )
+}
 
 export default ProductPage;
